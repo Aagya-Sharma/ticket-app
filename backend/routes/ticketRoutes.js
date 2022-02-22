54d31c85efb3fc2f5617606dada852eb96ac4f24
Aagya-Sharma <sharmaaagya7@gmail.com>
2022-02-22 09:30:08 +0545

    node functionality added

--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -2,6 +2,12 @@ const express = require('express')
 const router = express.Router()
 const {getTickets,createTicket, getTicket,deleteTicket,updateTicket} = require('../controllers/ticketControllers')
 
+
+
+// Re-route into note router
+const noteRouter = require('./noteRoutes')
+router.use('/:ticketId/notes', noteRouter)
+
 const {protect} = require('../middleware/authMiddleware')
 
 router.route('/').get(protect,getTickets).post(protect,createTicket)
